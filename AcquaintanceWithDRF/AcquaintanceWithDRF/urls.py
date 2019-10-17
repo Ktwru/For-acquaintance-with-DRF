5ce99996591451477bd9f6afd5256a7ca8865a95
Ktwru <spacewarlushd@gmail.com>
2019-10-17 23:14:10 +0300

    ListApi

--- AcquaintanceWithDRF/AcquaintanceWithDRF/urls.py
+++ AcquaintanceWithDRF/AcquaintanceWithDRF/urls.py
@@ -19,5 +19,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/cars', include('cars.urls')),
+    path('api/v1/cars/', include('cars.urls')),
 ]
